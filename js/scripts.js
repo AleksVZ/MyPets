fcd97212de044ceb33f15e5e348bf6e2e2c99839
AleksVZ <zaiko1984@ukr.net>
2022-12-11 16:44:17 +0200

    add animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        nav: false,
-        dots:false,
-        navText: [ '', ' ' ],
+//     $('.owl-carousel').owlCarousel({
+//         loop:true,
+//         margin:0,
+//         autoplay:true,
+//         nav: false,
+//         dots:false,
+//         navText: [ '', ' ' ],
 
-        responsive:{
-            0:{
-                items:1
-            },
-            1030:{
-                items:1
-            }
-        }
-    });
+//         responsive:{
+//             0:{
+//                 items:1
+//             },
+//             1030:{
+//                 items:1
+//             }
+//         }
+//     });
 
-});
+// });
 
 $('.toggle').click(function(e) {
     e.preventDefault();
@@ -35,3 +35,20 @@ $('.toggle').click(function(e) {
       $this.next().slideToggle(350);
   }
 });
+
+// Прозрачные буквы
+// $(document).ready(function(){
+//     var mouseX, mouseY;
+//     var ww = $( window ).width();
+//     var wh = $( window ).height();
+//     var traX, traY;
+//     $(document).mousemove(function(e){
+//       mouseX = e.pageX;
+//       mouseY = e.pageY;
+//       traX = ((4 * mouseX) / 570) + 40;
+//       traY = ((4 * mouseY) / 570) + 50;
+//       console.log(traX);
+//       $(".title").css({"background-position": traX + "%" + traY + "%"});
+//     });
+//   });
+
