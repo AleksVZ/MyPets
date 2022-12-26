9cde425a806b26c65486641a0ca13209475749c3
AleksVZ <zaiko1984@ukr.net>
2022-12-26 12:40:13 +0200

    add filter Dogs_Catalog

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,22 @@ $('.toggle').click(function(e) {
 //     });
 //   });
 
+//?    search Letter
+
+function searchDogs(){
+    let input, filter, ul, li, a, i;
+    input = document.getElementById('inputSearchDogs');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('letter');
+    li = document.getElementsByTagName('li');
+
+    for(i=0; i<li.length; i++){
+        a =li[i].getElementsByTagName('a')[0];
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        }
+        else{
+            li[i].style.display = "none";
+        }
+    }
+}
